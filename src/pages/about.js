b4dac9565537f1a3f6ec060883cdc3463164f4ae
firtysh <sm2450@it.jgec.ac.in>
2023-01-02 23:05:50 +0530

    about alomst complete

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles/about.css'
-
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import SchoolIcon from '@mui/icons-material/School';
 function About() {
   return (
     <section id='about'>
@@ -14,6 +15,7 @@ function About() {
           </h2>
           <p className='glass'>
             JWOC provides a fully immersive learning experience for students and first-time contributors by promoting the wonders of open-source software and crafting a community of new and experienced technical developers. The best projects are selected for this program. Students get acquainted with the projects from the mentors during the Community Bonding Period. Students work on these projects during the coding phase. At the end of the coding period, the winners of the programs are announced on the basis of their contribution in terms of quantity as well as quality.
+          <button className="btn">Code Of Conduct</button>
           </p>
         </div>
         <div className="info-container glass">
@@ -27,15 +29,21 @@ function About() {
           <h1 className='glass'>INFO</h1>
           <div className="info-cards">
             <div className="info-card glass">
-              <i>ICON</i>
-              Mentors Manual
+            <AccountBoxIcon  sx={{
+              fontSize: 50,
+            }}/>
+            <span className="info-card-title">Mentor's Manual</span>
+              
               <button className="btn">
                 Get Here
               </button>
             </div>
             <div className="info-card glass">
-            <i>ICON</i>
-              Student Manual
+            <SchoolIcon sx={{
+              fontSize: 50,
+            }}/>
+            <span className="info-card-title">Student's Manual</span>
+              
               <button className="btn">
                 Get Here
               </button>
